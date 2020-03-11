98378d4eef23f823b1720d3734790f07fae2217d
Adam Rudow <arudow09@gmail.com>
2020-03-10 23:21:13 -0700

    Added remaining projects to premake file. Trying to get d3d included/linked properly for BlitzRenderer

--- premake5.lua
+++ premake5.lua
@@ -8,10 +8,22 @@ workspace "BlitzEngine"
 		"Release",
 		"AssetProcessor",
 	}
+	
+	characterset ("MBCS")
 
-	---"Debug/Windows/x64"
+	---"Debug/Windows-x64"
 outputDir = "%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}"
-
+d3d_SDK_includePath = "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/include"
+d3d_SDK_libPath     = "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib/%{cfg.architecture}"
+fbx_SDK_includePath     = "C:/Program Files/Autodesk/FBX/FBX SDK/2018.1.1/include"
+fbx_SDK_libPath_debug   = "C:/Program Files/Autodesk/FBX/FBX SDK/2018.1.1/lib/vs2015/%{cfg.architecture}/debug"
+fbx_SDK_libPath_release = "C:/Program Files/Autodesk/FBX/FBX SDK/2018.1.1/lib/vs2015/%{cfg.architecture}/release"
+windows_SDK_includePath =
+{
+	"C:/Program Files (x86)/Windows Kits/8.1/Include/um",
+	"C:/Program Files (x86)/Windows Kits/8.1/Include/shared",
+	"C:/Program Files (x86)/Windows Kits/8.1/Include/winrt"
+}
 --[postbuildcommands
 --{
 --	("{COPY} %{cfg.buildtarget.relpath} ../Output-p/Binaries/" .. outputDir .. "/FeatureTestGame")
@@ -40,7 +52,7 @@ project "BlitzCommon"
 
 	includedirs
 	{
-		"%{prj.name}",
+		"Engine/%{prj.name}/Source",
 		"External/spdlog/include",
 	}
 
@@ -56,7 +68,7 @@ project "BlitzCommon"
 			"_COMMON_DLL",
 		}
 
-		
+		disablewarnings { "4996" }
 
 	filter "configurations:Debug"
 		defines ""
@@ -71,8 +83,244 @@ project "BlitzCommon"
 		symbols "On"
 		optimize "On"
 
+		
+-----------------------------------------------------------------
+---------------        BlitzSystem      -------------------------
+-----------------------------------------------------------------
+project "BlitzSystem"
+	location "Engine/BlitzSystem"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir  ("Output-p/Binaries/" .. outputDir)
+	objdir ("Output-p/Intermediate/" .. outputDir)
+
+	files
+	{
+		"Engine/%{prj.name}/Source/**.h",
+		"Engine/%{prj.name}/Source/**.cpp",
+		"Engine/%{prj.name}/Source/Components/**.h",
+		"Engine/%{prj.name}/Source/Components/**.cpp",
+		"Engine/%{prj.name}/Source/Game/**.h",
+		"Engine/%{prj.name}/Source/Game/**.cpp",
+		"Engine/%{prj.name}/Source/Rendering/**.h",
+		"Engine/%{prj.name}/Source/Rendering/**.cpp",
+	}
+
+	includedirs
+	{
+		"Engine/%{prj.name}/Source",
+		"Engine/BlitzCommon/Source",
+		"External/spdlog/include",
+	}
+	
+	links
+	{
+		"BlitzCommon.lib",
+	}
+
+	libdirs { "Output-p/Binaries/" .. outputDir }
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		--systemversion "8.1"
+
+		defines
+		{
+			"_WINDLL",
+			"_SYSTEMDLL",
+		}
+
+		disablewarnings { "4996" }
+
+	filter "configurations:Debug"
+		defines ""
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines ""
+		optimize "On"
+		
+	filter "configurations:AssetProcessor"
+		defines ""
+		symbols "On"
+		optimize "On"
+
+-----------------------------------------------------------------
+---------------        BlitzRenderer      -------------------------
+-----------------------------------------------------------------
+project "BlitzRenderer"
+	location "Engine/BlitzRenderer"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir  ("Output-p/Binaries/" .. outputDir)
+	objdir ("Output-p/Intermediate/" .. outputDir)
+
+	files
+	{
+		"Engine/%{prj.name}/Source/**.h",
+		"Engine/%{prj.name}/Source/**.cpp",
+		"Engine/%{prj.name}/Source/FW1FontWrapper/**.h",
+		"Engine/%{prj.name}/Source/FW1FontWrapper/**.cpp",
+	}
+
+	includedirs
+	{
+		"Engine/%{prj.name}/Source",
+		"Engine/BlitzCommon/Source",
+		"Engine/BlitzSystem/Source",
+		"External/spdlog/include",
+		d3d_SDK_includePath,
+		windows_SDK_includePath,
+	}
+	
+	links
+	{
+		"BlitzCommon.lib",
+		"BlitzSystem.lib",
+	}
+	
+	libdirs
+	{
+		"Output-p/Binaries/" .. outputDir,	
+		d3d_SDK_libPath,
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		--systemversion "8.1"
+
+		defines
+		{
+			"_WINDLL",
+			"_SYSTEMDLL",
+		}
+
+		disablewarnings { "4996" }
+
+	filter "configurations:Debug"
+		defines ""
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines ""
+		optimize "On"
+		
+	filter "configurations:AssetProcessor"
+		defines ""
+		symbols "On"
+		optimize "On"
+
+		
+-----------------------------------------------------------------
+---------------        AssetProcessor    -------------------------
+-----------------------------------------------------------------
+project "AssetProcessor"
+	location "Engine/AssetProcessor"
+	language "C++"
+
+	targetdir  ("Output-p/Binaries/" .. outputDir)
+	objdir ("Output-p/Intermediate/" .. outputDir)
+
+	files
+	{
+		"Engine/%{prj.name}/Source/**.h",
+		"Engine/%{prj.name}/Source/**.cpp",
+	}
+
+	includedirs
+	{
+		"Engine/%{prj.name}/Source",
+		"External/spdlog/include",
+		d3d_SDK_includePath,
+		fbx_SDK_includePath,
+		windows_SDK_includePath,
+		"Engine/BlitzSystem",
+		"Engine/BlitzCommon",
+	}
+
+	links
+	{
+		"BlitzCommon.lib",
+		"BlitzSystem.lib",
+		"BlitzRenderer.lib",
+		"libfbxsdk.lib",
+	}
+	
+	libdirs
+	{
+		"Output-p/Binaries/" .. outputDir,		
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		--systemversion "8.1"
+
+		defines
+		{
+			"WIN32",
+			"FBXSDK_SHARED",
+			"_CONSOLE",
+			
+		}
+
+		disablewarnings { "4996" }
+
+	filter "configurations:Debug"
+		kind "StaticLib"
+		symbols "On"
+		defines
+		{
+			"_DEBUG",
+			"_ASSET_LOADER_DLL",
+			"_COMMON_DLL",
+			"_MATHDLL",
+			"_RENDERERDLL",
+			"_SYSTEMDLL",
+		}
+		libdirs
+		{
+			fbx_SDK_libPath_release		
+		}
+		
+	filter "configurations:Release"
+		kind "StaticLib"
+		optimize "On"
+		defines
+		{
+			"_ASSET_LOADER_DLL",
+			"_COMMON_DLL",
+			"_MATHDLL",
+			"_RENDERERDLL",
+			"_SYSTEMDLL",
+		}
+		libdirs
+		{
+			fbx_SDK_libPath_release		
+		}
+		
+	filter "configurations:AssetProcessor"
+		kind "ConsoleApp"
+		symbols "On"
+		optimize "On"
+		defines
+		{
+			"_DEBUG",
+			"_ASSET_PROCESSOR",
+		}
+		libdirs
+		{
+			fbx_SDK_libPath_debug	
+		}
 
 
+-----------------------------------------------------------------
+-------------------         Games      --------------------------
+-----------------------------------------------------------------
 project "FeatureTestGame"
 	location "Games/FeatureTestGame"
 	kind "ConsoleApp"
@@ -91,20 +339,22 @@ project "FeatureTestGame"
 
 	includedirs
 	{
-		"%{prj.name}",
+		"Games/%{prj.name}/Source",
 		"External/spdlog/include",
-		--"Engine\BlitzSystem",
+		"Engine/BlitzSystem",
 		"Engine/BlitzCommon",
 	}
 
 	links
 	{
 		"BlitzCommon.lib",
-		--"BlitzSystem",
-		--"BlitzRenderer",
+		"BlitzSystem.lib",
+		"BlitzRenderer.lib",
 	}
 
-	libdirs { "libs", "Output-p/Binaries/" .. outputDir }
+	libdirs { "Output-p/Binaries/" .. outputDir }
+	
+	disablewarnings { "4996" }
 
 	filter "system:windows"
 		cppdialect "C++17"
@@ -125,4 +375,6 @@ project "FeatureTestGame"
 	filter "configurations:Release"
 		defines ""
 		optimize "On"
+		
+		
 	
